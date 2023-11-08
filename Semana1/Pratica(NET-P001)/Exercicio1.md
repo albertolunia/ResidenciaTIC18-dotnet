ca7838b8303c13d16ad82680638ec901a5c26389
albertolunia <albertolunia@gmail.com>
2023-11-08 14:39:52 -0300

    Subindo 1 e 2 da ativadade

---
+++ Semana1/Pratica(NET-P001)/Exercicio1.md
@@ -0,0 +1,11 @@
+# Verificando a instalação do .NET SDK
+
+Para verificar se o .NET SDK está corretamente instalado em seu sistema, você deve abrir o terminal ou prompt de comando e digitar o comando `dotnet --version` e pressione Enter. Isso exibirá a versão do .NET SDK instalada no seu sistema.
+
+# Removendo uma versão específica do .NET SDK
+
+Se você precisar remover uma versão específica do .NET SDK, pode usar o comando `dotnet sdk remove <version>`. Substitua `<version>` pela versão que deseja remover.
+
+# Atualizando o .NET SDK para a versão mais recente
+
+Para atualizar o .NET SDK para a versão mais recente, você pode usar o comando `dotnet tool update -g dotnet`. Isso atualizará o .NET SDK globalmente em seu sistema.
---
+++ Semana1/Pratica(NET-P001)/Program.cs
@@ -0,0 +1,43 @@
+﻿#region tipos de dados
+
+sbyte valorSByte = 25;
+byte valorByte = 10;
+short valorShort = 30000;
+ushort valorUShort= 60000;
+int valorInt = 50000;
+uint valorUint= 100000;
+long valorLong = 7800000000;
+ulong valorULong = 15600000000;
+float valorFloat = 4.5f;
+double valorDouble= 4.5;
+decimal valorDecimal = 4.5m;
+
+Console.WriteLine("Valor SByte: " + valorSByte);
+Console.WriteLine("Valor Byte: " + valorByte);
+Console.WriteLine("Valor Short: " + valorShort);
+Console.WriteLine("Valor UShort: " + valorUShort);
+Console.WriteLine("Valor Int: " + valorInt);
+Console.WriteLine("Valor UInt: " + valorUint);
+Console.WriteLine("Valor Long: " + valorLong);
+Console.WriteLine("Valor ULong: " + valorULong);
+Console.WriteLine("Valor Float: " + valorFloat);
+Console.WriteLine("Valor Double: " + valorDouble);
+Console.WriteLine("Valor Decimal: " + valorDecimal);
+
+#endregion
+
+#region conversão de tipos de dados
+
+//A solução que imaginei foi separar a parte inteira da parte decimal e guardar em duas variaveis, pois a conversão direta perde a o valor da casa decimal
+int parteInteira;
+double parteDecimal;
+
+valorDouble = 4.5;
+
+parteInteira = (int)Math.Truncate(valorDouble);
+parteDecimal = valorDouble - parteInteira;
+
+Console.WriteLine("Parte inteira: " + parteInteira);
+Console.WriteLine("Parte decimal: " + parteDecimal);
+
+#endregion
