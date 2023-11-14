03c2bab63be470e74434e05d35f001aef6901fb5
alberto <albertolunia@gmail.com>
2023-11-14 12:14:26 -0300

    Exercicios aula4, pendente ex4

--- Semana2/Exercicio-Aula4/Program.cs
+++ Semana2/Exercicio-Aula4/Program.cs
@@ -16,3 +16,50 @@ Console.WriteLine("Mes: " + mes);
 Console.WriteLine("Ano: " + ano);
 
 #endregion
+
+#region Exercicio 2
+
+Console.WriteLine("\nExercicio 2\n");
+
+int[] numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+foreach (int numero in numeros)
+{
+    if (numero % 2 == 0)
+        Console.WriteLine(numero);
+}
+
+#endregion
+
+#region Exercicio 3
+
+Console.WriteLine("\nExercicio 3\n");
+
+List<string> cidades = new List<string>();
+
+cidades.Add("São Paulo");
+cidades.Add("Rio de Janeiro");
+cidades.Add("Belo Horizonte");
+cidades.Add("Curitiba");
+cidades.Add("Porto Alegre");
+
+foreach (string cidade in cidades)
+{
+    if(cidade.StartsWith("S"))
+        Console.WriteLine(cidade);
+}
+
+#endregion
+
+#region Exercicio 4
+
+Console.WriteLine("\nExercicio 4\n");
+
+string dataHoraAtual, dataHoraFuturo;
+
+dataHoraAtual = "01/01/2024 12:00";
+dataHoraFuturo = "02/01/2024 12:00";
+
+
+
+#endregion
