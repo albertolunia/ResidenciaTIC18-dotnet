8a2b3fb240008e3a5432a4cf9be17b23550f6e82
albertolunia <albertolunia@gmail.com>
2023-11-22 09:28:02 -0300

    Exericio lambda aula 5

--- Semana3/Exercicio-Aula5/Program.cs
+++ Semana3/Exercicio-Aula5/Program.cs
@@ -1,4 +1,4 @@
-﻿#region tuple exemplo
+﻿#region Tuple Exemples
 
 var tuple1 = (id: 1, name: "Everaldina", born: new DateTime(2001, 7, 12));
 
@@ -6,5 +6,13 @@ List<(int id, string name, DateTime born)> list = new();
 list.Add(tuple1);
 list.Add((2, "Alberto", new DateTime(1999, 2, 14)));
 list.ForEach(x => Console.WriteLine($"Tuple 2: {x.id} - {x.name} - {x.born.ToShortDateString()}"));
+
+#endregion
+
+#region LambdaExpression Exemples
+
+string[] people = { "Alberto", "Everaldina", "João", "Maria", "José" };
+char letter = 'A';
+Console.WriteLine($"\nPeople that the name contains '{letter}': {string.Join(", ", people.Where(x => x.Contains(letter)))}");
 
 #endregion
