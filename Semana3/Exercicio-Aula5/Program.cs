dc792fda103f752d2492c5b8779940aec59c5354
albertolunia <albertolunia@gmail.com>
2023-11-22 12:22:57 -0300

    Exercicio5 da aula 5

--- Semana3/Exercicio-Aula5/Program.cs
+++ Semana3/Exercicio-Aula5/Program.cs
@@ -1,44 +1,76 @@
-﻿#region Tuple Exemples
+﻿// #region Tuple Exemples
 
-var tuple1 = (id: 1, name: "Everaldina", born: new DateTime(2001, 7, 12));
+// var tuple1 = (id: 1, name: "Everaldina", born: new DateTime(2001, 7, 12));
 
-List<(int id, string name, DateTime born)> list = new();
-list.Add(tuple1);
-list.Add((2, "Alberto", new DateTime(1999, 2, 14)));
-list.ForEach(x => Console.WriteLine($"Tuple 2: {x.id} - {x.name} - {x.born.ToShortDateString()}"));
+// Console.WriteLine("--- Tuple Exemples ---\n");
 
-#endregion
+// List<(int id, string name, DateTime born)> list = new();
+// list.Add(tuple1);
+// list.Add((2, "Alberto", new DateTime(1999, 2, 14)));
+// list.ForEach(x => Console.WriteLine($"Tuple 2: {x.id} - {x.name} - {x.born.ToShortDateString()}"));
 
-#region LambdaExpression Exemples
+// #endregion
 
-string[] people = { "Alberto", "Everaldina", "João", "Maria", "José" };
-char letter = 'A';
-char letter2 = 'a';
-Console.WriteLine($"\nPeople that the name contains '{letter}': {string.Join(", ", people.Where(x => x.Contains(letter) || x.Contains(letter2)))}");
+// #region LambdaExpression Exemples
 
-#endregion
+// Console.WriteLine("\n--- LambdaExpression Exemples --- \n");
 
-#region LINQ Exemples
+// string[] people = { "Alberto", "Everaldina", "João", "Maria", "José" };
+// char letter = 'A';
+// char letter2 = 'a';
+// Console.WriteLine($"People that the name contains '{letter}': {string.Join(", ", people.Where(x => x.Contains(letter) || x.Contains(letter2)))}");
 
-Console.WriteLine("\nLINQ Exemples: \n");
+// #endregion
 
-var students = new List<Student>{
-   new Student(1, "Alberto Henrique", "123456789", new DateTime(1999, 2, 14), new List<string>{"123456789", "73988342986"}),
-   new Student(2, "Andreia Miranda", "987654321", new DateTime(1981, 9, 1), new List<string>{"123456789", "73987654321"}),
-   new Student(3, "Alberto Jesus", "123456789", new DateTime(1974, 10, 2), new List<string>{"123456789", "77988237086"})
-};
+// #region LINQ Exemples
 
-var any = students.Any();
-var anyHelder = students.Any(x => x.FullName.Contains("Helder"));
-//var single = students.Single(x => x.Id == 10);
-//throw exception
-var singleOrDefault = students.SingleOrDefault(x => x.Id == 10);
+// Console.WriteLine("\n--- LINQ Exemples ---\n");
 
-var select = students.Select(x => x.PhoneNumbers);
-var selectMany = students.SelectMany(x => x.PhoneNumbers);
+// var students = new List<Student>{
+//    new Student(1, "Alberto Henrique", "123456789", new DateTime(1999, 2, 14), new List<string>{"123456789", "73988342986"}),
+//    new Student(2, "Andreia Miranda", "987654321", new DateTime(1981, 9, 1), new List<string>{"123456789", "73987654321"}),
+//    new Student(3, "Alberto Jesus", "123456789", new DateTime(1974, 10, 2), new List<string>{"123456789", "77988237086"})
+// };
 
-var legalAge = students.Where(x => x.BirthDate <= DateTime.Today.AddYears(-18)).Select(x => x.FullName);
-Console.WriteLine($"Legal age people: {string.Join(", ", legalAge)}");
+// var any = students.Any();
+// var anyHelder = students.Any(x => x.FullName.Contains("Helder"));
+// //var single = students.Single(x => x.Id == 10);
+// //throw exception
+// var singleOrDefault = students.SingleOrDefault(x => x.Id == 10);
+
+// var select = students.Select(x => x.PhoneNumbers);
+// var selectMany = students.SelectMany(x => x.PhoneNumbers);
+
+// var legalAge = students.Where(x => x.BirthDate <= DateTime.Today.AddYears(-18)).Select(x => x.FullName);
+// Console.WriteLine($"Legal age people: {string.Join(", ", legalAge)}");
+
+// #endregion
+
+#region Pratica
+
+Console.WriteLine("\n--- Pratica ---\n");
+
+Console.WriteLine("Insira um número: ");
+try
+{
+   int number = int.Parse(Console.ReadLine()!);
+}
+catch (Exception ex)
+{
+   Console.WriteLine("O valor inserido não é um número!");
+   Console.WriteLine(ex.Message);  
+}
+
+Console.WriteLine("Insira um número: ");
+int result = Divide(10, 0);
+
+int Divide(int number1, int number2)
+{
+   if (number2 == 0)
+      throw new DivideByZeroException("Não é possível dividir por zero!");
+
+   return number1 / number2;
+}
 
 #endregion
  public class Student{
---
+++ Semana3/Exercicio5-Aula5/Pessoa.cs
@@ -0,0 +1,11 @@
+namespace Exercicio5_Aula5;
+public class Pessoa
+{
+
+    public string Nome { get; set; }
+    public double Altura { get; set; }
+    public Pessoa(string nome, double altura){
+        Nome = nome;
+        Altura = altura;
+    }
+}
