f9af8024543ff761318155a6e05ea2d66abba468
albertolunia <ahmjesus.cic@uesc.br>
2023-11-29 10:41:14 -0300

    Commit 15 minutos 2

---
+++ Avaliacao/ConsultorioMedico/Program.cs
@@ -0,0 +1,36 @@
+﻿using ConsultorioMedico;
+
+class Programa{
+    public static void Main(string[] args)
+    {
+        Requisitos requisitos = new();
+        Paciente paciente = new();
+        Medico medico = new();
+
+        paciente.Nome = "João";
+        paciente.DataNascimento = new(2000, 1, 1);
+        paciente.Cpf = "99999999999";
+        paciente.Sexo = "M";
+        paciente.Sintomas = "Dor de cabeça";
+
+        medico.Nome = "José";
+        medico.DataNascimento = new(1990, 1, 1);
+        medico.Cpf = "12345678910";
+        medico.Crm = "123456";
+
+        requisitos.CadastrarPaciente(paciente);
+        requisitos.CadastrarMedico(medico);
+
+        requisitos.ListarPacientes();
+        requisitos.ListarMedicos();
+
+        //Testando se insere pacientes com o mesmo CPF
+
+        requisitos.CadastrarPaciente(paciente);
+
+        //Testando se insere medicos com o mesmo CPF e CRM
+
+        requisitos.CadastrarMedico(medico);
+
+    }
+}
