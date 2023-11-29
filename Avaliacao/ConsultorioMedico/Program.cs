db3051969277a53d813014b89c97d95d06b2a3fb
albertolunia <ahmjesus.cic@uesc.br>
2023-11-29 11:45:45 -0300

    Commit 15 minutos 6

--- Avaliacao/ConsultorioMedico/Program.cs
+++ Avaliacao/ConsultorioMedico/Program.cs
@@ -9,42 +9,51 @@ class Programa{
         Medico medico = new();
 
         //Testando se insere pacientes e medicos
-        paciente.Nome = "João";
-        paciente.DataNascimento = "29111999";
-        paciente.Cpf = "99999999999";
-        paciente.Sexo = "M";
-        paciente.Sintomas = "Dor de cabeça";
-
-        Console.WriteLine(paciente.Nome);
-        requisitos.CadastrarPaciente(paciente);
-
-        paciente.Nome = "Maria";
-        paciente.DataNascimento = "21022001";
-        paciente.Cpf = "99993999999";
-        paciente.Sexo = "F";
-        paciente.Sintomas = "Colica";
+        paciente = new Paciente{
+            Nome = "João",
+            DataNascimento = "29111999",
+            Cpf = "99999999999",
+            Sexo = "M",
+            Sintomas = "Dor de cabeça e Gripe"
+        };
 
         requisitos.CadastrarPaciente(paciente);
 
-        paciente.Nome = "Pedro";
-        paciente.DataNascimento = "30012013";
-        paciente.Cpf = "99999999991";
-        paciente.Sexo = "M";
-        paciente.Sintomas = "Dor de barriga";
+        paciente = new Paciente{
+            Nome = "Maria",
+            DataNascimento = "21022001",
+            Cpf = "99993999999",
+            Sexo = "F",
+            Sintomas = "Colica"
+        };
 
         requisitos.CadastrarPaciente(paciente);
 
-        medico.Nome = "José";
-        medico.DataNascimento = "29111980";
-        medico.Cpf = "12345678910";
-        medico.Crm = "123456";
+        paciente = new Paciente{
+            Nome = "Alberto",
+            DataNascimento = "30012013",
+            Cpf = "99993999991",
+            Sexo = "M",
+            Sintomas = "Dor de barriga"
+        };
+
+        requisitos.CadastrarPaciente(paciente);
+
+        medico = new Medico{
+            Nome = "José",
+            DataNascimento = "29111980",
+            Cpf = "12345678910",
+            Crm = "123456"
+        };
 
         requisitos.CadastrarMedico(medico);
 
-        medico.Nome = "Ana";
-        medico.DataNascimento = "21022001";
-        medico.Cpf = "12345678911";
-        medico.Crm = "123457";
+        medico = new Medico{
+            Nome = "Ana",
+            DataNascimento = "21022001",
+            Cpf = "12345678911",
+            Crm = "123457"
+        };
 
         requisitos.CadastrarMedico(medico);
 
@@ -64,5 +73,24 @@ class Programa{
         medico.Cpf = "12312312312";
         requisitos.CadastrarMedico(medico);
 
+        Console.WriteLine("\n---------------------------------------------\n");
+        Console.WriteLine("Relatorios:\n");
+        Console.WriteLine("Relatorio: Médicos com idades entre dois valores:");
+        requisitos.MedicosEntreIdade(20, 30);
+
+        Console.WriteLine("\nRelatorio: Pacientes com idades entre dois valores:");
+        requisitos.PacientesEntreIdade(20, 30);
+
+        Console.WriteLine("\nRelatorio: Pacientes do sexo masculino:\n");
+        requisitos.PacientesDoSexo("M");
+
+        Console.WriteLine("\nLista de Pacientes Ordenada");
+        requisitos.ListarPacientesEmOrdem();
+
+        Console.WriteLine("\nPacientes com sintomas de dor de cabeça:\n");
+        requisitos.PacientesComSintomas("Dor de cabeça");
+
+        Console.WriteLine("\nRelatorio: Aniversariantes do mês:\n");
+        requisitos.AniversariantesDoMes();
     }
 }
