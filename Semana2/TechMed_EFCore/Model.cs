1b8da94c7f6aefa8c9604b8e48bd08e151452e62
albertolunia <albertolunia@gmail.com>
2024-01-11 10:37:37 -0300

    Aula 2

--- Semana2/TechMed_EFCore/Model.cs
+++ Semana2/TechMed_EFCore/Model.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore;
 namespace TechMed_EFCore.Models;
 public class TechMedContext : DbContext
 {
+    public DbSet<Medico> Medicos { get; set; }
+    public DbSet<Paciente> Pacientes { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         base.OnConfiguring(optionsBuilder);
@@ -10,4 +12,29 @@ public class TechMedContext : DbContext
         var serverVersion = ServerVersion.AutoDetect(connectionString);
         optionsBuilder.UseMySql(connectionString, serverVersion);
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Medico>().ToTable("Medicos").HasKey(m => m.Id);
+        modelBuilder.Entity<Paciente>().ToTable("Pacientes").HasKey(p => p.Id);
+    }
+}
+
+public abstract class Pessoa{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string CPF { get; set; }
+    
+}
+
+public class Medico : Pessoa{
+    public string CRM { get; set; }
+    public string Especialidade { get; set; }
+    public string Salario { get; set; }
+}
+
+public class Paciente : Pessoa{
+    public string Endereco { get; set; }
+    public string Telefone { get; set; }
 }
--- Semana2/TechMed_EFCore/Program.cs
+++ Semana2/TechMed_EFCore/Program.cs
@@ -1,3 +1,15 @@
 ﻿using TechMed_EFCore.Models;
 
 var context = new TechMedContext();
+
+// context.Medicos.Add(new Medico{
+//     Nome = "Dr. House",
+//     CPF = "123.456.789-00",
+//     CRM = "123456",
+//     Especialidade = "Clínico Geral",
+//     Salario = "R$ 10.000,00"
+// });
+
+// context.SaveChanges();
+
+context.Medicos.ToList().ForEach(m => Console.WriteLine($"{m.Id} - {m.Nome} - {m.CRM} - {m.Especialidade} - {m.Salario}"));
